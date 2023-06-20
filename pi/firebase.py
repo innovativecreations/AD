402017b88a5337bdc420b76ca4f9703e06220316
Mayank <innovativecreations195@gmail.com>
2023-06-20 11:51:18 +0530

    removing files which are not present on firebase

--- pi/firebase.py
+++ pi/firebase.py
@@ -1,18 +1,23 @@
 from google.cloud import storage
 import os
-os_vid = os.listdir("vid")
+
 # print(os_vid[0])
 # Initialize Google Cloud Storage client
 client = storage.Client.from_service_account_json('key.json')  # Replace with the path to your key file
 bucket_name = 'ad-vid.appspot.com'  # Replace with the name of your Firebase Storage bucket
 while True :
+    os_vid = os.listdir("vid")
     bucket = client.bucket(bucket_name)
     blobs = bucket.list_blobs()
 
 # Print the names of all the files
-
+    fVList = []
     for blob in blobs:
-        if not blob.name in os_vid:
+        fVList.append(blob.name)
+        if blob.name in os_vid:
+            print("Already Exists")
+        else:
+            
             print(blob.name)
             blob = bucket.blob(blob.name)
             # Specify the local path where you want to save the downloaded video
@@ -22,5 +27,13 @@ while True :
             blob.download_to_filename(local_path)
     
             print(f"Video downloaded successfully to: {local_path}")
+            
+    print(fVList)
+    for vid in os_vid:
+
+        if vid in fVList:
+            
+            print("Not this file")
         else:
-            print("all set")
+            os.remove("vid/" + vid)
+            print("removed" + vid)
